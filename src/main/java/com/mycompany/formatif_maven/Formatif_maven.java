4cc2f59ceb73f6ceae4b55304253a392690554c6
Mohamed Amine Hanou <2417033@grasset.qc.ca>
2025-02-19 18:35:29 -0500

    Initialisation du projet Maven

---
+++ src/main/java/com/mycompany/formatif_maven/Formatif_maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formatif_maven;
+
+/**
+ *
+ * @author 2417033
+ */
+public class Formatif_maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
